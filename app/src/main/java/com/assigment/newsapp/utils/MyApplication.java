c3d00cae708e2e779d45f67ba431480e2e5f8e12
Tarun A <tarunanchala@gmail.com>
2020-09-22 20:10:23 +0530

    Initial Commit for News App

--- app/src/main/java/com/assigment/newsapp/utils/MyApplication.java
+++ app/src/main/java/com/assigment/newsapp/utils/MyApplication.java
@@ -27,18 +27,4 @@ public class MyApplication extends Application {
 
 }
 
-//public class MyApplication extends Application {
-//    public static MyApplication instance;
-//    @Override
-//    public void onCreate() {
-//        super.onCreate();
-//        instance = this;
-//    }
-//    @Override
-//    public Context getApplicationContext() {
-//        return super.getApplicationContext();
-//    }
-//    public static MyApplication getInstance() {
-//        return instance;
-//    }
-//}
+
